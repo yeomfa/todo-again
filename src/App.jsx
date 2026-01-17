d3299302785429e02edb39fc84321e92ea6f1231
yeomfa <yeomfa@gmail.com>
2026-01-16 21:52:36 -0500

    Implement todo functionality with local storage and enhance TodoItem interactions

--- src/App.jsx
+++ src/App.jsx
@@ -22,27 +22,62 @@ const defaultTodos = [
 ];
 
 function App() {
+  const storagedTodos = localStorage.getItem('todos_v1');
+  let parsedTodos = [];
+
+  if (!storagedTodos) {
+    localStorage.setItem('todos_v1', JSON.stringify(defaultTodos));
+    parsedTodos = defaultTodos;
+  } else {
+    parsedTodos = JSON.parse(localStorage.getItem('todos_v1'));
+  }
+
   const [searchValue, setSearchValue] = useState('');
-  const [todos, setTodos] = useState(defaultTodos);
+  const [todos, setTodos] = useState(parsedTodos);
 
   const totalTodos = todos.length;
   const completedTodos = todos.filter((todo) => todo.completed).length;
+  const searchedTodos = todos.filter((todo) =>
+    todo.text.toLowerCase().includes(searchValue.toLowerCase())
+  );
+
+  const saveTodos = (todos) => {
+    localStorage.setItem('todos_v1', JSON.stringify(todos));
+    setTodos(todos);
+  };
+
+  const completeTodo = (id) => {
+    const newTodos = todos.map((todo) => {
+      const newTodo = { ...todo };
+
+      newTodo.completed =
+        newTodo.id === id ? !newTodo.completed : newTodo.completed;
+
+      return newTodo;
+    });
+
+    saveTodos(newTodos);
+  };
+  const deleteTodo = (id) => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+
+    saveTodos(newTodos);
+  };
 
   return (
     <div className="todo-app">
       <TodoCounter completed={completedTodos} total={totalTodos} />
       <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
       <TodoList>
-        {todos.map((todo) => {
-          if (todo.text.toLowerCase().includes(searchValue.toLowerCase()))
-            return (
-              <TodoItem
-                key={todo.id}
-                text={todo.text}
-                completed={todo.completed}
-              />
-            );
-        })}
+        {searchedTodos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.id)}
+            onDelete={() => deleteTodo(todo.id)}
+          />
+        ))}
       </TodoList>
       <CreateTodoButton />
     </div>
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,14 +1,31 @@
-import { CheckCircleIcon, XCircleIcon } from '@phosphor-icons/react';
+import {
+  CheckCircleIcon,
+  MinusCircleIcon,
+  XCircleIcon,
+} from '@phosphor-icons/react';
 import '../assets/styles/TodoItem.css';
 
-export function TodoItem({ text, completed = false }) {
+export function TodoItem({
+  text,
+  completed = false,
+  onComplete = () => {},
+  onDelete = () => {},
+}) {
   const completedClass = completed ? 'completed' : '';
 
   return (
     <li className={`todo-item ${completedClass}`}>
-      <CheckCircleIcon className="todo-icon" size={20} weight="fill" />
+      <button className="todo-icon" onClick={onComplete}>
+        {completed ? (
+          <MinusCircleIcon size={20} weight="fill" />
+        ) : (
+          <CheckCircleIcon size={20} weight="fill" />
+        )}
+      </button>
       <p className="todo-text">{text}</p>
-      <XCircleIcon className="todo-icon" size={20} weight="fill" />
+      <button onClick={onDelete} className="todo-icon">
+        <XCircleIcon size={20} weight="fill" />
+      </button>
     </li>
   );
 }
